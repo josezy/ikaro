4dc617fcd9ec19f464394f6a800b82e142002157
JoseB <jose.zdy@gmail.com>
2019-06-14 23:06:49 -0500

    setup js environment. create simple react page/component. compile and bundle it

---
+++ ikarodjango/js/components/maps.js
@@ -0,0 +1,78 @@
+import React, {PureComponent} from 'react';
+import ReactMapGL, {Marker, NavigationControl, GeolocateControl} from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1Ijoiam9zZXp5IiwiYSI6ImNqd3ZmdGFmbzA4dGQ0OW41em5reDU3cmMifQ.5Ab2UzBgSWEoRgjxN9fMhg"
+
+
+const ICON = `
+M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,
+0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,
+0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9C20.1,15.8,
+20.2,15.8,20.2,15.7z
+`
+
+const pinStyle = {
+    fill: '#d00',
+    stroke: 'none',
+    cursor: 'pointer'
+}
+
+class Pin extends PureComponent {
+    render() {
+        const {size = 20, onClick} = this.props;
+
+        return (
+            <svg height={size} viewBox="0 0 24 24" onClick={onClick} style={{
+              ...pinStyle,
+              transform: `translate(${-size / 2}px,${-size}px)`
+            }}>
+               <path d={ICON} />
+            </svg>
+        )
+    }
+}
+
+
+const navStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    padding: '10px'
+}
+
+export class MapContainer extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            viewport: {
+                width: '100%',
+                height: '100%',
+                latitude: 40.7,
+                longitude: -74.1,
+                zoom: 8
+            }
+        }
+    }
+
+    updateViewport(viewport) {
+        this.setState({viewport})
+    }
+
+    render() {
+        return <ReactMapGL
+                  {...this.state.viewport}
+                  mapStyle="mapbox://styles/mapbox/streets-v8"
+                  mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+                  onViewportChange={(viewport) => this.setState({viewport})}
+                >
+
+            <div style={navStyle}>
+                <NavigationControl onViewportChange={::this.updateViewport} />
+            </div>
+
+        </ReactMapGL>
+    }
+}
+
