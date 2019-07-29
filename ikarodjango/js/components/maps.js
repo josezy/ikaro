87d3767cceb7338977050dc8f1bd7548bdcedcf8
JoseB <jose.zdy@gmail.com>
2019-07-29 11:54:34 -0500

    bye uber's "react-map-gl". Hi alex's "react-mapbox-gl"

--- ikarodjango/js/components/maps.js
+++ ikarodjango/js/components/maps.js
@@ -1,57 +1,39 @@
-import React, {PureComponent} from 'react';
-import ReactMapGL, {Marker, NavigationControl, GeolocateControl} from 'react-map-gl';
+import React, {PureComponent} from 'react'
+import ReactMapboxGl, {
+    Marker, RotationControl, ZoomControl
+} from 'react-mapbox-gl'
 
-import 'mapbox-gl/dist/mapbox-gl.css'
 
+const Mapbox = ReactMapboxGl({
+    accessToken: global.props.map_key,
+})
 
-const navStyle = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    padding: '10px'
-}
 
 export class MapContainer extends PureComponent {
     constructor(props) {
         super(props)
-        this.state = {
-            viewport: {
-                width: '100%',
-                height: '100%',
-                latitude: 6.147674,
-                longitude: -75.393580,
-                zoom: 16
-            }
-        }
-    }
-
-    updateViewport(viewport) {
-        this.setState({viewport})
+        this.state = {}
     }
 
     render() {
         const {center} = this.props
 
-        return <ReactMapGL
-                  {...this.state.viewport}
-                  mapStyle="mapbox://styles/mapbox/navigation-guidance-night-v4"
-                  mapboxApiAccessToken={global.props.map_api}
-                  onViewportChange={(viewport) => this.setState({viewport})}
-                >
-
-            <div style={navStyle}>
-                <NavigationControl onViewportChange={::this.updateViewport} />
-            </div>
-            {center &&
-                <Marker latitude={center.lat} longitude={center.lon} >
-                    <img
-                        src="/static/img/map_marker.png"
-                        style={{transform: "translate(-50%, -50%)"}}
-                    />
-                </Marker>
-            }
-
-        </ReactMapGL>
+        return (
+            <Mapbox
+                style="mapbox://styles/mapbox/navigation-guidance-night-v4"
+                center={center || [-75.393921, 6.149080]}
+                zoom={[16]}
+                className="mapbox-component"
+            >
+                <RotationControl />
+                <ZoomControl />
+                {center &&
+                    <Marker coordinates={center} anchor="center">
+                        <img src="/static/img/map_marker.png" />
+                    </Marker>
+                }
+            </Mapbox>
+        )
     }
 }
 
