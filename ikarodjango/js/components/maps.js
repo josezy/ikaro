2331b3cf3fecd550e79df627c926bbc455f9cd93
JoseB <jose.zdy@gmail.com>
2019-07-28 23:45:25 -0500

    use custom image instead of svg

--- ikarodjango/js/components/maps.js
+++ ikarodjango/js/components/maps.js
@@ -4,36 +4,6 @@ import ReactMapGL, {Marker, NavigationControl, GeolocateControl} from 'react-map
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 
-
-const ICON = `
-M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,
-0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,
-0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9C20.1,15.8,
-20.2,15.8,20.2,15.7z
-`
-
-const pinStyle = {
-    fill: '#d00',
-    stroke: 'none',
-    cursor: 'pointer'
-}
-
-class Pin extends PureComponent {
-    render() {
-        const {size = 20, onClick} = this.props;
-
-        return (
-            <svg height={size} viewBox="0 0 24 24" onClick={onClick} style={{
-              ...pinStyle,
-              transform: `translate(${-size / 2}px,${-size}px)`
-            }}>
-               <path d={ICON} />
-            </svg>
-        )
-    }
-}
-
-
 const navStyle = {
     position: 'absolute',
     top: 0,
@@ -64,7 +34,7 @@ export class MapContainer extends PureComponent {
 
         return <ReactMapGL
                   {...this.state.viewport}
-                  mapStyle="mapbox://styles/mapbox/streets-v8"
+                  mapStyle="mapbox://styles/mapbox/navigation-guidance-night-v4"
                   mapboxApiAccessToken={global.props.map_api}
                   onViewportChange={(viewport) => this.setState({viewport})}
                 >
@@ -74,7 +44,10 @@ export class MapContainer extends PureComponent {
             </div>
             {center &&
                 <Marker latitude={center.lat} longitude={center.lon} >
-                    <Pin />
+                    <img
+                        src="/static/img/map_marker.png"
+                        style={{transform: "translate(-50%, -50%)"}}
+                    />
                 </Marker>
             }
 
