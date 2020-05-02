e33768769b34b30f99ad745017645e9391f46041
Jose B <jose.zdy@gmail.com>
2020-05-01 21:15:12 -0500

    better handle logic

--- ikarodjango/js/components/command_sender.js
+++ ikarodjango/js/components/command_sender.js
@@ -9,32 +9,38 @@ export class CommandSender {
         this.commands = commands
         this.cmd_index = 0
         this.unsubscribe = this.store.subscribe(this._subscribe.bind(this))
+        this._send_command()
+    }
+    _send_command() {
         this.store.dispatch(send_mavcmd(
             this.commands[this.cmd_index].command,
             this.commands[this.cmd_index].params
         ))
     }
     _subscribe() {
+        if (this.cmd_index >= this.commands.length || this.cmd_index < 0) this._end()
+
         const state = this.store.getState()
         const current_ack = state.mavlink && state.mavlink.COMMAND_ACK
+        const ack_command = current_ack && MAVLINK_IDS[current_ack.command]
 
-        if (
-            current_ack
-            && MAVLINK_IDS[current_ack.command] == this.commands[this.cmd_index].command
-            && current_ack.result == 0
-        ) {
-            this.cmd_index += 1
-            if (this.cmd_index < this.commands.length) {
-                this.store.dispatch(send_mavcmd(
-                    this.commands[this.cmd_index].command,
-                    this.commands[this.cmd_index].params
-                ))
+        if (current_ack && ack_command == this.commands[this.cmd_index].command) {
+            if (current_ack.result == 0) {
+                this.cmd_index += 1
+                if (-1 < this.cmd_index && this.cmd_index < this.commands.length) {
+                    this._send_command()
+                } else {
+                    this._end()
+                }
             } else {
-                this.unsubscribe()
-                this.cmd_index = -1
-                this.commands = []
-                this.unsubscribe = () => {}
+                // TODO: handle other ACK results
+                this._end()
             }
         }
+    }
+    _end() {
+        this.unsubscribe()
+        this.cmd_index = -1
+        this.commands = []
     }
 }
