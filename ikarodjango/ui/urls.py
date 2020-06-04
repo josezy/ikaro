8323e713d3213cad8461ead88055a5cec13171ec
Jose B <jose.zdy@gmail.com>
2020-06-04 17:40:23 -0500

    disable signup

--- ikarodjango/ui/urls.py
+++ ikarodjango/ui/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('', Looby.as_view(), name='home'),
 
     path('accounts/login/', Login.as_view(), name='login'),
-    path('accounts/signup/', Signup.as_view(), name='signup'),
+    path('accounts/signup/', lambda r: 'Error', name='signup'),
+    path('genesis/', Signup.as_view()),
     path('accounts/logout/', Logout.as_view(), name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
 
