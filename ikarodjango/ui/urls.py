27e08ee432112ac28ff507e56fd17e29d0bcab54
Jose B <jose.zdy@gmail.com>
2020-06-04 17:44:30 -0500

    fix http response

--- ikarodjango/ui/urls.py
+++ ikarodjango/ui/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.http import HttpResponse
 
 from ui.views.pages import Looby, FlightPanel
 from ui.views.accounts import Login, Logout, Signup
@@ -8,7 +9,8 @@ urlpatterns = [
     path('', Looby.as_view(), name='home'),
 
     path('accounts/login/', Login.as_view(), name='login'),
-    path('accounts/signup/', lambda r: 'Error', name='signup'),
+    path('accounts/signup/', lambda r: HttpResponse(
+        "Error", content_type="text/plain"), name='signup'),
     path('genesis/', Signup.as_view()),
     path('accounts/logout/', Logout.as_view(), name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
