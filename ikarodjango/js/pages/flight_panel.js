62539a2c35b42f2eeba84520bd804f654f88b765
Jose B <jose.zdy@gmail.com>
2021-04-11 14:53:32 -0500

    hide eye

--- ikarodjango/js/pages/flight_panel.js
+++ ikarodjango/js/pages/flight_panel.js
@@ -66,7 +66,7 @@ export const FlightPanel = {
                 <TukanoPanel />
                 {global.props.is_pilot && <Controls />}
                 <Indicators />
-                <SpectatorsEye />
+                {/* <SpectatorsEye /> */}
                 <NerdInfo />
             </Provider>
         )
