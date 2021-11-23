e4d4369cddd0decc18bf79b7709fc8b0821fbc05
abcei2 <santiago.salgado.duque@gmail.com>
2021-11-22 19:03:48 -0500

    .

--- ikarodjango/js/gcs/manual_control/gamepad_cursor.js
+++ ikarodjango/js/gcs/manual_control/gamepad_cursor.js
@@ -10,9 +10,9 @@ export function GamepadCursor(props, ref) {
     let throttle_range=Math.abs(props.vehicleParams.throttleParams.maxPwm-props.vehicleParams.throttleParams.minPwm)
     let roll_range=Math.abs(props.vehicleParams.rollParams.maxPwm-props.vehicleParams.rollParams.minPwm)
     let left_vertical = Math.abs(gamepads[gamepadId].axes[1])
-    let left_horizontal = Math.abs(gamepads[gamepadId].axes[0])
+    let left_horizontal = Math.abs(gamepads[gamepadId].axes[2])
    
- 
+    console.log(gamepads[gamepadId].axes)
     if(props.takeControlFlag){
         if(gamepads[gamepadId].axes[1]<0){
             props.vehicleParams.throttleParams.orientation = 0
@@ -21,6 +21,7 @@ export function GamepadCursor(props, ref) {
         }
         props.vehicleParams.throttleParams.throttle = Math.floor(props.vehicleParams.throttleParams.minPwm+left_vertical*throttle_range)
         props.vehicleParams.rollParams.roll = Math.floor(1500+left_horizontal*roll_range/2)
+        
     }
   });
   return (
