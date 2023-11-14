73942d84caa73ce73910558c33d17c7e6a6bec66
Jose B <jose.zdy@gmail.com>
2023-11-14 17:20:27 -0500

    fix docker compose setup

--- ikarodjango/ikaro/routing.py
+++ ikarodjango/ikaro/routing.py
@@ -2,11 +2,13 @@ from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 
 from panel.consumers import MavlinkConsumer
 
 application = ProtocolTypeRouter({
-    'websocket': AllowedHostsOriginValidator(
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
                 path("mavlink/<str:type>/<str:id>", MavlinkConsumer.as_asgi()),
